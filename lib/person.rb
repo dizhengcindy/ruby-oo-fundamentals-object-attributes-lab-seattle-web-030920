d3c59217911b3fdd82dedcecb5ecc6f30e17d10e
Di Zheng <dizhengcindy@gmail.com>
2020-03-11 03:44:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,5 @@
 class Person 
   
-   def name=(name)
-  @name = name
-  end
-  def name 
-  @name = name 
-  end
-  
-   def job = (job)
-  @job = job
-  end
-  
-  def job
-    @job 
-  end
+ 
 
 end
